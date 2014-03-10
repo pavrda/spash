b479161b95b4860a6887a1c451933ce11669149b
Jarda <all@pavrda.cz>
2014-03-10 14:22:00 +0100

    asdasa

--- www/js/index.js
+++ www/js/index.js
@@ -27,12 +27,14 @@ var app = {
     // 'load', 'deviceready', 'offline', and 'online'.
     bindEvents: function() {
         document.addEventListener('deviceready', this.onDeviceReady, false);
+        alert('bind1');
     },
     // deviceready Event Handler
     //
     // The scope of 'this' is the event. In order to call the 'receivedEvent'
     // function, we must explicity call 'app.receivedEvent(...);'
     onDeviceReady: function() {
+    	alert('devReady1');
         app.receivedEvent('deviceready');
     },
     // Update DOM on a Received Event
